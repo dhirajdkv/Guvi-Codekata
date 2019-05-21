a342bf8aadd11621048229e33003a68fd8b99d40
dhirajdkv <50867441+dhirajdkv@users.noreply.github.com>
2019-05-21 15:27:14 +0530

    Add files via upload

---
+++ set2.py
@@ -0,0 +1,11 @@
+a = int(input())
+if a>=0:
+    if a%2 == 0:
+        print("Even")
+    elif a==0:
+        print("Even")
+    elif a%2 == 1:
+        print("Odd")
+else:
+    print("Invalid")
+    
