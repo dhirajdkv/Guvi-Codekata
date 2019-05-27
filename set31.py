e1b04da7ba79500d272cf736403a636e63db4f8c
dhirajdkv <50867441+dhirajdkv@users.noreply.github.com>
2019-05-27 15:11:49 +0530

    Add files via upload

---
+++ set31.py
@@ -0,0 +1,7 @@
+a = input()
+a = a.split()
+count = 0
+for item in a:
+    count = count + len(item)
+print(count)
+    
---
+++ set32.py
@@ -0,0 +1,6 @@
+a = input()
+a = a.split()
+count = 0
+for item in a:
+    count = count + 1
+print(count)
