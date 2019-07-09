267ba28cf0f4dfbbf9bd3a56b016c486bb1e3371
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-09 15:49:22 +0530

    Create uniquenum.cpp

---
+++ uniquenum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+  int num;
+  int a=0,b=0,temp=0;
+  cout<<"enter how many numbers"<<endl;
+  cin>>num;
+  int arr[num];
+  cout<<"enter the numbers"<<endl;
+  for(int i=0;i<num;i++){
+    cin>>arr[i];
+  }
+   for(int i=0; i<num; i++){
+    for(int j=i+1;j<num;j++){
+      if(arr[i]>arr[j]){
+        temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  for(int i=0;i<num;i++){
+    for(int j=i+1;j<num;j++){
+      if(arr[i]==arr[j]){
+        if(arr[i]>0){
+          cout<<arr[i]<<" ";
+        }
+        arr[j]=-1;
+        break;
+      }
+    }
+  }
+
+}
