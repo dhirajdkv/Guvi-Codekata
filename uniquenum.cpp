f2c566779f6df2965f59abfc25a274b380e72c91
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-09 16:05:14 +0530

    Update uniquenum.cpp

--- uniquenum.cpp
+++ uniquenum.cpp
@@ -4,8 +4,10 @@ using namespace std;
 int main(){
   int num;
   int a=0,b=0,temp=0;
+  cout<<"enter how many numbers"<<endl;
   cin>>num;
   int arr[num];
+  cout<<"enter the numbers"<<endl;
   for(int i=0;i<num;i++){
     cin>>arr[i];
   }
@@ -18,16 +20,18 @@ int main(){
       }
     }
   }
-  for(int i=0;i<num;i++){
+  bool bol=false;
+  for(int i=0;i<num-1;i++){
     for(int j=i+1;j<num;j++){
       if(arr[i]==arr[j]){
-        if(arr[i]>0){
-          cout<<arr[i]<<" ";
-        }
         arr[j]=-1;
-        break;
+        bol=true;
       }
     }
+      if(bol && arr[i]>0){
+        cout<<arr[i];
+        bol=false;
+      }
   }
 
 }
