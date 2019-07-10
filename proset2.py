af1bbb03c63eb02f8955bcae3c82ad5162faf441
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-10 15:41:35 +0530

    Update proset2.py

--- proset2.py
+++ proset2.py
@@ -2,9 +2,15 @@ name1,name2=input().split()
 count=0
 namelen1=len(name1)
 namelen2=len(name2)
-for i in range(0,namelen1):
+if(namelen1<=namelen2):
+    mainlen=namelen1
+    maxlen=namelen2
+else:
+    mainlen=namelen2
+    maxlen=namelen1
+for i in range(0,mainlen):
     if(name1[i]==name2[i]):
         count+=1
     else:
         break
-print(namelen2-count)
+print(maxlen-count)
