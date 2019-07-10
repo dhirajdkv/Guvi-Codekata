1b1770dfc0b57dac84a3a3e4679a99182dff7305
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-10 09:26:09 +0530

    Update set9.py

--- set9.py
+++ set9.py
@@ -1,10 +1,10 @@
 n = int(input())
 k = int(input())
 s = 0
-a = []
+b = []
 for i in range(0,n):
     v =int(input())
-    a.append(v)
+    b.append(v)
 for i in range(0,k):
-    s = s + a[i]
+    s = s + b[i]
 print(s)
