fb844ab5688bdc9d2bc6c744a9e4019ea2721ada
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-10 10:06:21 +0530

    Update set16.py

--- set16.py
+++ set16.py
@@ -1,12 +1,13 @@
-a = int(input())
-b = int(input())
-for i in range(a,b+1):
+a,b=input().split()
+a=int(a)
+b=(int)(b)
+for i in range(a,b):
     if i>1:
         for j in range(2,i):
             if i%j==0:
                 break
         else:
-            print(i)
+            print(i,end=' ')
 
 
         
