e9555f2f45e2aecab38b51786972c6e5799c1b19
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-11 16:17:16 +0530

    Create firstnumberrepeated.cpp

---
+++ firstnumberrepeated.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main(){
+  int num;
+  cin>>num;
+  int number[num];
+  for(int i=0;i<num;i++){
+    cin>>number[i];
+  }
+  bool flag=true;
+  for(int i=0;i<num;i++){
+      for(int j=i+1;j<num;j++)
+      {
+        if(number[i]==number[j])
+        {
+          cout<<number[i];
+          flag=false;
+          break;
+        }
+      }
+      if(flag==false){
+        break;
+      }
+    }
+  }
